a5886d36a6bf011ea0fd01792f85dbe5dd017abd
Joakim Holmgren <joakim.holmgren@comaround.com>
2020-02-25 09:25:40 +0100

    drag and drop

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { moveItemInArray, CdkDragMove, CdkDropList, CdkDropListGroup, CdkDrag, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { moveItemInArray, CdkDragMove, CdkDropList, CdkDropListGroup, CdkDrag, CdkDragDrop, transferArrayItem } from '@angular/cdk/drag-drop';
 
 
 @Component({
@@ -10,21 +10,17 @@ import { moveItemInArray, CdkDragMove, CdkDropList, CdkDropListGroup, CdkDrag, C
 export class AppComponent {
   title = 'dragdrop';
 
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi'
+  @ViewChild(CdkDropListGroup) listGroup: CdkDropListGroup<CdkDropList>;
+
+    timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Early modern period',
+    'Long nineteenth century'
   ];
 
-  @ViewChild(CdkDropListGroup) listGroup: CdkDropListGroup<CdkDropList>;
-  @ViewChild(CdkDropList) placeholder: CdkDropList;
-
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
   }
 }
